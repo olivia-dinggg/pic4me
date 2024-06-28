39b4cfa76279e14cfb068da33141932fa7fc327c
Aditya <121993233+xadta@users.noreply.github.com>
2024-06-28 20:01:56 +1000

    Added files

---
+++ frontend/app/components/objects/PostDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
+// import CloseIcon from '@mui/icons-material/Close';
+import { Box, FormControl, InputAdornment, MenuItem, Modal, TextField, Typography, Button } from '@mui/material';
+
+const PostDisplay = () => {
+
+    return <></>;
+}
