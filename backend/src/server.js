f3bec856e7f6584aefaee009452d49e65789d0c4
Edward Roy <z5419523@unsw.edu.au>
2024-06-28 20:23:48 +1000

    Added photo posting and retrieving http endpoints/functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,13 @@ import config from './config.json' assert { type: "json" };
 import express, { json } from 'express';
 import cors from 'cors';
 import errorHandler from './middleware.js';
+import { getData, loadData } from './datastore.js';
 
 const app = express();
 app.use(json());
 app.use(cors());
+
+loadData();
 
 const PORT = parseInt(process.env.PORT || config.port);
 const HOST = process.env.IP || 'localhost';
@@ -13,7 +16,31 @@ const HOST = process.env.IP || 'localhost';
 // API Endpoints
 app.get('/data', (req, res) => {
   res.json(['ex', 'as'])
-})
+});
+
+app.get('/photo', (req, res) => {
+    // Extracting user and photoID from query parameters
+    const userId = req.query.userId;
+    const photoId = req.query.photoId;
+
+    // Check if both parameters are present
+    if (!userId || !photoId) {
+        return res.status(400).json({ error: 'Both userId and photoId parameters are required.' });
+    }
+
+    res.json(getPhoto(userId, photoId));
+});
+
+
+app.post('/photo', (req, res) => {
+    const { userId, encodedPhoto, dateTaken, prompt } = req.body;
+
+    if (!userId || !encodedPhoto || !dateTaken || !prompt) {
+        return res.status(400).json({ error: 'userId, encodedPhoto, dateTaken and prompt parameters are required for POST request.' });
+    }
+
+    res.json(postPhoto(userId, encodedPhoto, dateTaken, prompt));
+});
 
 // Middleware must exist AFTER all API endpoints.
 app.use(errorHandler());
