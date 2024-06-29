220f54c870b8dfc64f7c1bcfbf809bb211e4aead
Edward Roy <z5419523@unsw.edu.au>
2024-06-29 16:04:11 +1000

    Created promp bar with image and default paraeters

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,17 +1,35 @@
 import { StyleSheet, Text, View } from "react-native";
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import PromptBar from "@/components/PromptBar";
+import { Box, Paper, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>hurehvuiehhuiuhiuhiuhiuhuhuihuih.</Text>
-    </View>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Paper sx={{ bgcolor: "#1f1f1f", height: '100vh', width: '100vw', color: 'white' }}>
+        <PromptBar />
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography sx={{ color: "#e8e8e8" }}>hurehvuiehhuiuhiuhiuhiuhuhuihuih.</Typography>
+
+        </View>
+      </ Paper>
+    </ThemeProvider>
+
   );
 }
 
--- frontend/components/PromptBar.tsx
+++ frontend/components/PromptBar.tsx
@@ -1,9 +1,51 @@
+import { Box, LinearProgress, Typography } from '@mui/material';
 import React from 'react'
 
 const PromptBar = () => {
+  const [data, setData] = React.useState({
+    category: 'none',
+    prompt: 'There is no promp at the moment. Good Work!'
+  });
 
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/prompt');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const categoryToImage = (category: string) => {
+    switch (category) {
+      case "physical":
+        return '../assets/images/exercise.png'
+      default:
+        return '../assets/images/thumbs-up.png'
+    }
+  }
+  var border = data.category === "none" ? 'none' : '3px outset #f2b022'
   return (
-    
+    <Box sx={{
+      bgcolor: '#121212',
+      border: border,
+      height: '12vh',
+      width: '100vw',
+      mb: '2vh',
+      display: 'inline-flex',
+      alignContent: 'center',
+    }}>
+      <img src={categoryToImage(data.category)} style={{ margin: '25px', marginLeft: 'auto' }}></img>
+      <Typography sx={{ my: 'auto', mr: 'auto' }}> {data.prompt} </Typography>
+    </Box>
   )
 }
 
